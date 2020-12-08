8879e25a3b6a573612f48d782381e78a1d1e0553
Peggy <peggy22509385@gmail.com>
2020-12-08 10:05:21 +0100

    order Hamburger

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        orderClass order = new orderClass();
+        order.selectHamburger();
+        System.out.println(order.printMenu());
+    }
+}
